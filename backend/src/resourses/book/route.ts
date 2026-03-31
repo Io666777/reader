c95a586b8eb881716302d67071668631b236595b
Io666777 <dimaskitihin@gmail.com>
2026-03-31 16:53:41 +0300

    feat/serch-start

--- backend/src/resourses/book/route.ts
+++ backend/src/resourses/book/route.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import * as controller from "./crud.controller"
+import * as serch from "../serch/serch.controller"
 
 const bookRoute = new Hono();
 bookRoute.get('/', controller.getAll);
@@ -7,5 +8,5 @@ bookRoute.get('/add', controller.addBook);
 bookRoute.delete('/:id', controller.deleteBook)
 
 
-
+bookRoute.get('/search', serch.searchExternal)
 export default bookRoute;
---
+++ backend/src/resourses/serch/serch.controller.ts
@@ -0,0 +1,33 @@
+import type { Context } from "hono";
+
+export const searchExternal = async (c: Context) => {
+  const title = c.req.query('title');
+  const author = c.req.query('author');
+
+  if (!title && !author) {
+    return c.json({ error: 'Нужен хотя бы один параметр: title или author' }, 400);
+  }
+
+  try {
+    // Формируем запрос: можно искать по title, по author или по обоим
+    let url = `https://openlibrary.org/search.json?limit=10`;
+    if (title) url += `&title=${encodeURIComponent(title)}`;
+    if (author) url += `&author=${encodeURIComponent(author)}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.docs.map((doc: any) => ({
+      isbn: doc.isbn?.[0] || doc.edition_key?.[0] || 'no-id',
+      title: doc.title,
+      author: doc.author_name?.[0] || 'Неизвестный автор',
+      year: doc.first_publish_year || '—',
+      cover: doc.cover_i ? `https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg` : null
+    }));
+
+    return c.json(results);
+  } catch (error) {
+    console.error("Search API Error:", error);
+    return c.json({ error: 'Ошибка внешнего поиска' }, 500);
+  }
+};
