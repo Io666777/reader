a4ad33c9f9b164e7a517eedfddb17ef9f9836806
Io666777 <dimaskitihin@gmail.com>
2026-04-01 16:03:18 +0300

    feat/front-import-sass-router

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      // Это позволит писать @/ вместо ../../../
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // Здесь можно подключить глобальные переменные (цвета, сетку)
+        // additionalData: `@import "@/app/styles/_variables.scss";`
+      }
+    }
+  }
 })
