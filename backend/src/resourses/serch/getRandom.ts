262d6bc8ac32b390d8f3c9cdce51712887c155db
Io666777 <dimaskitihin@gmail.com>
2026-05-04 17:31:24 +0300

    feat/back-update-random: изменение логики работы рандом метода(теперь по названию)

--- backend/src/resourses/serch/getRandom.ts
+++ backend/src/resourses/serch/getRandom.ts
@@ -1,24 +1,22 @@
 import { topics } from "../../utils/keyWord";
 import type { Context } from "hono";
 
-export const getRandom = async (c: Context) => {
-  try {
-   if (!topics || topics.length === 0) {
-      throw new Error("Массив тем пуст или не найден");
-    }
-    const randomTopic = topics[Math.floor(Math.random() * topics.length)];
-    const randomStart = Math.floor(Math.random() * 200);
+export const getRandom = async (c: Context): Promise<any> => {
+  const randomWord = topics[Math.floor(Math.random() * topics.length)];
+  const randomStart = Math.floor(Math.random() * 40);
 
-    const url = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(randomTopic)}&startIndex=${randomStart}&maxResults=1`;
+  const url = `https://www.googleapis.com/books/v1/volumes?q=intitle:${encodeURIComponent(randomWord)}&startIndex=${randomStart}&maxResults=1`;
 
-    const response = await fetch(url);
-    const data = await response.json();
-    return c.json({
-      book: data.items?.[0] || null,
-      metadata: { topic: randomTopic, index: randomStart, status: 'fallback_to_start' }
-    });
+  const response = await fetch(url);
+  const data = await response.json();
 
-  } catch (error: any) {
-    return c.json({ error: error.message }, 500);
+  if (!data.items || data.items.length === 0) {
+    console.log("Пусто, ищу заново...");
+    return getRandom(c); 
   }
+
+  return c.json({
+    book: data.items[0],
+    word: randomWord
+  });
 };
