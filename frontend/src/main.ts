170cfd097546a60733546eb9e89e305665bcaf3b
Io666777 <dimaskitihin@gmail.com>
2026-05-29 14:33:54 +0300

    feat/frontend: переход к фронт части

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia';
+import { clerkPlugin } from '@clerk/vue'
 import App from './app/App.vue'
 import router from './app/providers/router/index'
 
 const app = createApp(App)
 
 app.use(router) 
-app.use(createPinia());
+
+app.use(clerkPlugin, {
+  publishableKey: import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+})
+
 app.mount('#app') 
